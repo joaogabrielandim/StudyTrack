6e2b11ff6b716cd41a4db17d4582afef816c3397
João Gabriel Landim <joaogabrielandim2006@gmail.com>
2026-04-01 16:39:33 -0300

    Coloquei o log de sessoes de estudo

---
+++ js/task.js
@@ -0,0 +1,7 @@
+
+function adicionar_sessao() {
+    const nova_sessao = document.createElement("div");
+    nova_sessao.classList.add("estudos");
+
+    document.getElementById("sessao-estudos").appendChild(nova_sessao)
+}

8f62d4896a165b57562817aad5c9b5aaca21cc98
João Gabriel Landim <joaogabrielandim2006@gmail.com>
2026-04-20 16:04:11 -0300

    Conectei todas as telas a navbar usando react router

--- StudyTrackReact/src/App.jsx
+++ StudyTrackReact/src/App.jsx
@@ -3,11 +3,20 @@ import Navbar from "./components/Navbar.jsx"
 import Home from "./pages/home-page.jsx"
 import Task from "./pages/task-page.jsx"
 import Planner from "./pages/planner-page.jsx"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <Planner />
+    <BrowserRouter>
+      
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/tarefas" element={<Task/>} />
+        <Route path="/calendario" element={<Planner/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- StudyTrackReact/src/components/Navbar.jsx
+++ StudyTrackReact/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import "./Navbar.css"
 import homeicon from "../assets/homeicon.svg"
 import taskicon from "../assets/taskicon.svg"
 import calendaricon from "../assets/calendaricon.svg"
+import {Link} from "react-router-dom"
+
 
 function Navbar() {
     return(
@@ -10,10 +12,17 @@ function Navbar() {
                 <h1 className="titulo">StudyTrack</h1>
             </div>
             <nav>
-                <a href="../pages/task.html"> <img src={homeicon} className="icon"></img> Início</a>
-                <a href="#"> <img src={taskicon} className="icon"></img> Tarefas</a>
-                <a href="../pages/planner.html"> <img src={calendaricon} className="icon"></img> Calendário</a>
+                <Link to="/" > <img src={homeicon} className="icon"></img> Início</Link>
+                <Link to="/tarefas" > <img src={taskicon} className="icon"></img> Tarefas</Link>
+                <Link to="/calendario" > <img src={calendaricon} className="icon"></img> Calendário</Link>
             </nav>
+            <div class="perfil-usuario">
+            <div class="avatar">JG</div>
+            <div class="info-usuario">
+            <div class="nome">João Gadelha</div>
+            <div class="email">joaogadelha@gmail.com</div>
+          </div>
+        </div>
         </section>
     )
 }

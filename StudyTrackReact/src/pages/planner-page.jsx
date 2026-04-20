50b156802e784a548c9966d49a14e2261c9fe241
João Gabriel Landim <joaogabrielandim2006@gmail.com>
2026-04-20 19:41:55 -0300

    otimizei o codigo CSS da planner page

--- StudyTrackReact/src/pages/planner-page.jsx
+++ StudyTrackReact/src/pages/planner-page.jsx
@@ -8,25 +8,24 @@ function Planner() {
         <Navbar/>
         
         <div className="conteudo">
-
-            <div className="tarefas">
-            <h2>Suas tarefas</h2>
-            <div>[Colocar tarefas aqui]</div>
+            <div className="principal" >
+                <div className="tarefas">
+                    <h2>Suas tarefas</h2>
+                    <div>[Colocar tarefas aqui]</div>
+                </div>
+                <div className="eventos">
+                    <h2>Seus eventos</h2>
+                    <div>[Colocar eventos aqui]</div>
+                </div>
             </div>
-            <div className="eventos">
-            <h2>Seus eventos</h2>
-            <div>[Colocar eventos aqui]</div>
-            </div>
-            <div className="calendario">
-            <iframe
-                src="https://calendar.google.com/calendar/embed?src=SEU_CALENDARIO%40gmail.com&ctz=America%2FSao_Paulo"
-                style={{ border: 0 }}
-                width="800"
-                height="600"
-                frameBorder="0"
-                scrolling="no"
-                title="Google Calendar"
-            ></iframe>
+                <div className="calendario">
+                <iframe
+                    src="https://calendar.google.com/calendar/embed?src=SEU_CALENDARIO%40gmail.com&ctz=America%2FSao_Paulo"
+                    style={{ border: 0, height: "100%", width: "100%" }}
+                    frameBorder="0"
+                    scrolling="no"
+                    title="Google Calendar"
+                ></iframe>
             </div>
         </div>
         </div>

fc934126226f67d53358efaee7520d184c6a35de
Fernando05-dev <fernandofernandes01@icloud.com>
2026-04-22 14:30:42 -0300

    coloquei os botoes de logout e configuracoes

--- StudyTrackReact/src/components/Navbar.jsx
+++ StudyTrackReact/src/components/Navbar.jsx
@@ -2,29 +2,46 @@ import "./Navbar.css"
 import homeicon from "../assets/homeicon.svg"
 import taskicon from "../assets/taskicon.svg"
 import calendaricon from "../assets/calendaricon.svg"
-import {Link} from "react-router-dom"
-
+// Sugestão: adicione os ícones novos na sua pasta assets
+import settingsicon from "../assets/settingsicon.jpg"
+import logouticon from "../assets/logouticon.png"
+import { Link } from "react-router-dom"
 
 function Navbar() {
-    return(
-        <section id="idnavbar" className="navbar">
-            <div className="div-titulo">
-                <h1 className="titulo">StudyTrack</h1>
-            </div>
-            <nav>
-                <Link to="/" > <img src={homeicon} className="icon"></img> Início</Link>
-                <Link to="/tarefas" > <img src={taskicon} className="icon"></img> Tarefas</Link>
-                <Link to="/calendario" > <img src={calendaricon} className="icon"></img> Calendário</Link>
-            </nav>
-            <div class="perfil-usuario">
-            <div class="avatar">JG</div>
-            <div class="info-usuario">
-            <div class="nome">João Gadelha</div>
-            <div class="email">joaogadelha@gmail.com</div>
+  return (
+    <section id="idnavbar" className="navbar">
+      <div className="div-titulo">
+        <h1 className="titulo">StudyTrack</h1>
+      </div>
+
+      <nav>
+        <Link to="/"><img src={homeicon} className="icon" /> Início</Link>
+        <Link to="/tarefas"><img src={taskicon} className="icon" /> Tarefas</Link>
+        <Link to="/calendario"><img src={calendaricon} className="icon" /> Calendário</Link>
+      </nav>
+
+      <div className="perfil-usuario">
+        <div className="user-info-wrapper">
+          <div className="avatar">JG</div>
+          <div className="info-usuario">
+            <div className="nome">João Gadelha</div>
+            <div className="email">joaogadelha@gmail.com</div>
           </div>
         </div>
-        </section>
-    )
+
+        <div className="acoes-usuario">
+          <button className="botao-perfil">
+            <img src={settingsicon} className="icon-small" /> 
+            Configurações
+          </button>
+          <button className="botao-perfil botao-logout">
+            <img src={logouticon} className="icon-small" />
+            Sair da conta
+          </button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Navbar

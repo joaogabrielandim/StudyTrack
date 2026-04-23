7a37670bfa49f47b84bda784777d083a1e4a92f8
Fernando05-dev <fernandofernandes01@icloud.com>
2026-04-23 10:32:47 -0300

    consertei a merda do PL

--- StudyTrackReact/src/pages/task-page.jsx
+++ StudyTrackReact/src/pages/task-page.jsx
@@ -1,32 +1,39 @@
 import "./task-page.css"
 import Navbar from "../components/Navbar"
 
-
 function Task() {
-    return(
-        <div className="principal">
-        
-            <Navbar/>
-    
-        <div className="conteudo-principal">
-            <div className="cabecalho">
-            <div>
-                <h1>Tarefas</h1>
-                <p>Gerencie suas atividades de curso!</p>
-            </div>
-            </div>
-            <div className="box">
-            <div className="boxprogresso"></div>
-            <div className="boxhoras"></div>
-            <div className="boxlista"></div>
-            <div className="boxsessoes"></div>
-            <div className="boxconcluidas"></div>
-            <div className="boxsequencia"></div>
-            <div className="boxdistribuicao"></div>
-            <div className="horasdeestudo"></div>
-            <div className="graficos"></div>
-            </div>
-        </div>
+    return (
+        <div className="principal-task">
+            <Navbar />
+            
+            <main className="conteudo-principal-task">
+                <div className="cabecalho-task">
+                    <h1>Tarefas</h1>
+                    <p>Gerencie suas atividades de curso!</p>
+                </div>
+
+                <div className="box-task">
+             
+                    <div className="coluna-cards-task">
+                        <div className="boxprogresso-task">
+                            <h3>Suas tarefas</h3>
+                            <p>[Colocar tarefas aqui]</p>
+                        </div>
+                        
+                        <div className="boxhoras-task">
+                            <h3>Seus eventos</h3>
+                            <p>[Colocar eventos aqui]</p>
+                        </div>
+                    </div>
+
+                
+                    <div className="horasdeestudo-task">
+                        <div className="placeholder-calendario-task">
+                            Calendário
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }

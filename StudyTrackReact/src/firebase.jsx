491892d8a458b5842cc83244c935cf61623c97c5
Fernando05-dev <fernandofernandes01@icloud.com>
2026-05-25 08:30:01 -0300

    rezemos

--- StudyTrackReact/src/firebase.jsx
+++ StudyTrackReact/src/firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCom_s-cUmjedAbCtf8bxci-U1SETzAQGo",
-  authDomain: "studytrack-82d9b.firebaseapp.com",
-  projectId: "studytrack-82d9b",
-  storageBucket: "studytrack-82d9b.firebasestorage.app",
-  messagingSenderId: "39025001445",
-  appId: "1:39025001445:web:48812050ce49521a26e50c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
